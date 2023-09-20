8793cbdc80e865dfcf9ed85a5907c165d2a3afd8
scj0113 <scj0113@naver.com>
2023-09-21 08:51:14 +0900

    com

--- src/main/java/com/kh/youtube/controller/VideoController.java
+++ src/main/java/com/kh/youtube/controller/VideoController.java
@@ -1,24 +1,35 @@
 package com.kh.youtube.controller;
 
-import com.kh.youtube.domain.Video;
-import com.kh.youtube.domain.VideoComment;
-import com.kh.youtube.domain.VideoLike;
+import com.kh.youtube.domain.*;
 import com.kh.youtube.service.VideoCommentService;
 import com.kh.youtube.service.VideoLikeService;
 import com.kh.youtube.service.VideoService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/*")
+@Log4j2
+@CrossOrigin(origins = {"*"},maxAge = 6000)
 public class VideoController {
 
+    @Value("${spring.servlet.multipart.location}")  // applocation.properties에 있는 변수
+    private String uploadPath;
+
     @Autowired
-    private VideoService video;
+    private VideoService videoService;
 
     @Autowired
     private VideoLikeService videoLike;
@@ -30,32 +41,89 @@ public class VideoController {
     // 영상 전체 조회 : GET - http://localhost:8080/api/video
     @GetMapping("/video")
     public ResponseEntity<List<Video>> VideoList() {
-        return ResponseEntity.status(HttpStatus.OK).body(video.showAll());
+        return ResponseEntity.status(HttpStatus.OK).body(videoService.showAll());
     }
 
     // 영상 추가 : POST - http://localhost:8080/api/video
     @PostMapping("/video")
-    public ResponseEntity<Video> createVideo(@RequestBody Video vo) {
-        return ResponseEntity.status(HttpStatus.OK).body(video.create(vo));
+    public ResponseEntity<Video> createVideo(MultipartFile video, MultipartFile image, String title, String desc, String categoryCode) {
+        log.info("video : " + video);
+        log.info("image : " + image);
+        log.info("title : " + title);
+        log.info("desc : " + desc);
+        log.info("categoryCode : " + categoryCode);
+        // video_title, video_desc, video_url, video_photo, category_code
+        
+        // 업로드 처리
+        // 비디오의 실제 파일 이름
+        String originalVideo = video.getOriginalFilename();
+        String realVideo = originalVideo.substring(originalVideo.lastIndexOf("\\")+1);
+        log.info("realVideo : " + realVideo);
+
+        // 이미지의 실제 파일 이름
+        String originalImage = image.getOriginalFilename();
+        String realImage = originalImage.substring(originalImage.lastIndexOf("\\")+1);
+        log.info("realImage : " + realImage);
+
+        // UUID
+        String uuid = UUID.randomUUID().toString();
+
+        // 실제로 저장할 파일 명 (위치 포함)
+        String saveVideo = uploadPath + File.separator + uuid + "_" +realVideo;
+        Path pathVideo = Paths.get(saveVideo);
+
+        String saveImage = uploadPath + File.separator + uuid + "_" + realImage;
+        Path pathImage = Paths.get(saveImage);
+
+        try {
+            video.transferTo(pathVideo);
+            image.transferTo(pathImage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+        Video vo = new Video();
+        vo.setVideoTitle(title);
+        vo.setVideoDesc(desc);
+        vo.setVideoUrl(saveVideo);
+        vo.setVideoPhoto(saveImage);
+
+        Category category = new Category();
+        category.setCategoryCode(Integer.parseInt(categoryCode));
+        vo.setCategory(category);
+
+        Channel channel = new Channel();
+        channel.setChannelCode(22);
+        vo.setChannel(channel);
+
+        Member member = new Member();
+        member.setId("user1");
+        vo.setMember(member);
+
+
+       // return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.status(HttpStatus.OK).body(videoService.create(vo));
     }
 
 
     // 영상 수정 : PUT - http://localhost:8080/api/video
     @PutMapping("/video")
     public ResponseEntity<Video> updateVideo(@RequestBody Video vo) {
-        return ResponseEntity.status(HttpStatus.OK).body(video.update(vo));
+        return ResponseEntity.status(HttpStatus.OK).body(videoService.update(vo));
     }
 
     // 영상 삭제 : DELETE - http://localhost:8080/api/video/1
     @DeleteMapping("/video/{id}")
     public ResponseEntity<Video> deleteVideo(@PathVariable int id) {
-        return ResponseEntity.status(HttpStatus.OK).body(video.delete(id));
+        return ResponseEntity.status(HttpStatus.OK).body(videoService.delete(id));
     }
 
     // 영상 1개 조회 : GET - http://localhost:8080/api/video/1
     @GetMapping("/video/{id}")
     public ResponseEntity<Video> showVideo(@PathVariable int id) {
-        return ResponseEntity.status(HttpStatus.OK).body(video.show(id));
+        return ResponseEntity.status(HttpStatus.OK).body(videoService.show(id));
     }
 
     // 영상 1개에 따른 댓글 전체 조회 : GET - http://localhost:8080/api/video/1/comment
@@ -95,9 +163,9 @@ public class VideoController {
     }
 
     // 댓글 좋아요 추가 : POST - http://localhost:8080/api/video/comment/like
-    @PostMapping("/video/comment/like")
+
 
 
     // 댓글 좋아요 취소 : DELETE - http://localhost:8080/api/video/comment/like/1
-    @DeleteMapping("/video/comment/like/{id}")
+
 }
